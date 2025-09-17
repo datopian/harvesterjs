cb0063fe5766bee7f244d1db6fc4f73a72a320c2
William Lima <willy.lima89@gmail.com>
2025-09-17 10:07:48 -0100

    add ods harvester and schema

---
+++ src/schemas/ods.d.ts
@@ -0,0 +1,22 @@
+export interface ODSDataset {
+  id: string;
+  name: string;
+  assetType: string;
+  description?: string;
+  owner?: {
+    displayName: string;
+  };
+  blobFilename?: string;
+  blobId?: string;
+  category?: string;
+  metadata: {
+    attachments?: {
+      filename: string;
+      assetId?: string;
+      blobId?: string;
+      name: string;
+    }[];
+  };
+  viewType?: string;
+  tags?: string[]
+}
